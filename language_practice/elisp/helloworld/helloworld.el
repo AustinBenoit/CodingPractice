c13fc030b3476acf482b45f0220065cb33ca7c60
Austin Benoit <austinryanbenoit@gmail.com>
2023-06-16 09:15:49 -0400

    Started an area for elisp code
    Classic hello world for elisp.

---
+++ language_practice/elisp/helloworld/helloworld.el
@@ -0,0 +1,25 @@
+;;; helloworld.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2023 Austin Benoit
+;;
+;; Author: Austin Benoit <austinryanbenoit@gmail.com>
+;; Maintainer: Austin Benoit <austinryanbenoit@gmail.com>
+;; Created: June 15, 2023
+;; Modified: June 15, 2023
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
+;; Homepage: https://github.com/austinbenoit/helloworld
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+(princ "hello world")
+
+(provide 'helloworld)
+;;; helloworld.el ends here
